647bfc563b43265fca713caafd46ab0145052773
Felix Ebert <felix.ebert@if-core.de>
2014-07-03 15:15:43 +0200

    iscroll set active card

--- src/js/spots.js
+++ src/js/spots.js
@@ -10,6 +10,19 @@
             snapSpeed: 400,
             keyBindings: true
         });
+        var cardScroll = new IScroll('#test-spot', {
+            scrollX: false,
+            scrollY: true,
+            momentum: false,
+            snap: 'li',
+            scrollbar: true,
+            snapSpeed: 400,
+            keyBindings: true
+        });
+        cardScroll.on('scrollEnd', function () {
+            $('#test-spot').find('.card').removeClass('active');
+            $('#test-spot').find('.card-list').children().eq(this.currentPage.pageY).addClass('active');
+        });
         document.addEventListener('touchmove', function (e) {
             e.preventDefault();
         }, false);
