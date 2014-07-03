c38d494f7925a2be1a038cf99c97e848ce5460dd
Felix Ebert <felix.ebert@if-core.de>
2014-07-03 15:40:37 +0200

    iscroll pane config

--- src/js/spots.js
+++ src/js/spots.js
@@ -1,16 +1,13 @@
 (function (d3, app) {
     'use strict';
 
-    var init = function () {
-        new IScroll('.fullscreen-content', {
-            scrollX: true,
-            scrollY: false,
-            momentum: false,
-            snap: true,
-            snapSpeed: 400,
-            keyBindings: true
-        });
-        var cardScroll = new IScroll('#test-spot', {
+    var buildCardScroll = function (selector) {
+        var $element = $(selector);
+        var activateCard = function (index) {
+            $element.find('.card').removeClass('active');
+            $element.find('.card-list').children().eq(index).addClass('active');
+        };
+        var cardScroll = new IScroll(selector, {
             scrollX: false,
             scrollY: true,
             momentum: false,
@@ -20,10 +17,35 @@
             keyBindings: true
         });
         cardScroll.on('scrollEnd', function () {
-            $('#test-spot').find('.card').removeClass('active');
-            $('#test-spot').find('.card-list').children().eq(this.currentPage.pageY).addClass('active');
+            activateCard(this.currentPage.pageY);
         });
+        activateCard(0);
+        return cardScroll;
+    };
+
+    var init = function () {
+        var currentPaneId = 'spot-steinsfurt';
+        var cardScroll = buildCardScroll('#' + currentPaneId);
+        var paneScroll = new IScroll('.panes', {
+            scrollX: true,
+            scrollY: false,
+            momentum: false,
+            snap: true,
+            snapSpeed: 400,
+            keyBindings: true
+        });
+        paneScroll.on('scrollEnd', function () {
+            var paneId = $('.panes').find('.cards').eq(this.currentPage.pageX).attr('id');
+            if (paneId !== currentPaneId) {
+                currentPaneId = paneId;
+                cardScroll.scrollTo(0, 0, 0);
+                cardScroll.destroy;
+                cardScroll = buildCardScroll('#' + currentPaneId);
+            }
+        });
+
         document.addEventListener('touchmove', function (e) {
+            e
             e.preventDefault();
         }, false);
     };
