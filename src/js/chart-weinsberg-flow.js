dca471bc2476c2a3b3ef34c540d3ffafc391d507
Felix Ebert <felix.ebert@if-core.de>
2014-07-15 07:24:58 +0200

    weinsberg flow chart

--- src/js/chart-weinsberg-flow.js
+++ src/js/chart-weinsberg-flow.js
@@ -26,14 +26,14 @@
 
     var containerSelector = '#chart-weinsberg-flow';
     var build = function () {
-        var margin = {top: 10, right: 10, bottom: 10, left: 10},
+        var margin = {top: 25, right: 25, bottom: 25, left: 25},
             width = 300,
             height = 300,
             innerWidth = width - margin.left - margin.right,
             innerHeight = height - margin.top - margin.bottom,
             aspect = width / height,
             center = [innerWidth / 2, innerHeight / 2],
-            barMaxWidth = center[0] - 15,
+            barMaxWidth = center[0] - 10,
             $container = $(containerSelector);
 
         var scale = d3.scale.linear()
@@ -48,31 +48,35 @@
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        scale.domain([0, d3.max(data, function (d) {
+        var maxValue = d3.max(data, function (d) {
             return d.value;
-        })]);
+        });
+        scale.domain([0, maxValue]);
 
-        var generateBarX2 = function (d) {
+        var color = d3.scale.linear()
+            .domain([0, maxValue])
+            .range(["#7C8FDB", "#06458F"]);
+
+        var generateBarX = function (d, constant) {
             if (d.direction === "W") {
-                return center[0] - scale(d.value);
+                return center[0] - constant;
             }
             if (d.direction === "E") {
-                return center[0] + scale(d.value);
+                return center[0] + constant;
             }
             return center[0];
         };
-        var generateBarY2 = function (d) {
+        var generateBarY = function (d, constant) {
             if (d.direction === "N") {
-                return center[1] - scale(d.value);
+                return center[1] - constant;
             }
             else if (d.direction === "S") {
-                return center[1] + scale(d.value);
+                return center[1] + constant;
             }
             return center[1];
         };
 
         var circleRadiusList = [(innerWidth / 2), (innerWidth / 3), 25];
-
         containerGroup.selectAll(".windrose")
             .data(circleRadiusList)
             .enter()
@@ -84,15 +88,97 @@
             .attr("cx", center[0])
             .attr("cy", center[1]);
 
+        var directionLabelList = [
+            {
+                label: 'Neuenstadt',
+                x: width / 2,
+                y: 15,
+                anchor: 'middle'
+            },
+            {
+                label: 'Schwabbach',
+                x: width - 15,
+                y: height / 2,
+                anchor: 'middle',
+                rotate: 90
+            },
+            {
+                label: 'Pleidelsheim',
+                x: width / 2,
+                y: height,
+                anchor: 'middle'
+            },
+            {
+                label: 'Neckarsulm',
+                x: 15,
+                y: height / 2,
+                anchor: 'middle',
+                rotate: 270
+            }
+        ];
+
+        svg.selectAll('.direction')
+            .data(directionLabelList)
+            .enter()
+            .append("text")
+            .attr("class", "direction")
+            .attr("x", function (d) {
+                return d.x;
+            })
+            .attr("y", function (d) {
+                return d.y;
+            })
+            .attr("text-anchor", function (d) {
+                return d.anchor;
+            })
+            .attr("transform", function (d) {
+                return d.rotate ? "rotate(" + d.rotate + " " + d.x + " " + d.y + ")" : "";
+            })
+            .text(function (d) {
+                return d.label;
+            });
+
         containerGroup.selectAll(".bar")
             .data(data)
             .enter()
             .append("line")
             .attr("class", "bar")
-            .attr("x1", center[0])
-            .attr("y1", center[1])
-            .attr("x2", generateBarX2)
-            .attr("y2", generateBarY2);
+            .attr("x1", function (d) {
+                return generateBarX(d, 7);
+            })
+            .attr("y1", function (d) {
+                return generateBarY(d, 7);
+            })
+            .attr("x2", function (d) {
+                return generateBarX(d, scale(d.value) + 7);
+            })
+            .attr("y2", function (d) {
+                return generateBarY(d, scale(d.value) + 7);
+            })
+            .style("stroke", function (d) {
+                return color(d.value);
+            });
+
+        containerGroup.selectAll(".orientation-line")
+            .data(data)
+            .enter()
+            .append("line")
+            .attr("class", "orientation-line")
+            .attr("x1", function (d) {
+                return generateBarX(d, 0);
+            })
+            .attr("y1", function (d) {
+                return generateBarY(d, 0);
+            })
+            .attr("x2", function (d) {
+                return generateBarX(d, 20);
+            })
+            .attr("y2", function (d) {
+                return generateBarY(d, 20);
+            })
+            .style("stroke", function (d) {
+                return color(d.value);
+            });
 
         var onResize = function () {
             var targetWidth = $container.width();
