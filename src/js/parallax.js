c5f95c11efb54a1cb3f8bec4d72239c268bfff77
Felix Ebert <felix.ebert@if-core.de>
2014-07-17 01:13:46 +0200

    parallax scrolling prototype

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -2,35 +2,11 @@
     'use strict';
 
     var init = function () {
-        var $window = $(window);
-        var $panes = $('.pane');
-        var $body = $('body');
-
-        //FadeIn all sections
         setTimeout(function () {
-
-            // Resize sections
-            adjustWindow();
-
-            // Fade in sections
-            $body.removeClass('loading').addClass('loaded');
-
-        }, 800);
-
-        function adjustWindow() {
-            // Init Skrollr
             var s = skrollr.init({
                 forceHeight: false
             });
-
-            // Resize our slides
-            $panes.each(function (index, pane) {
-                $(pane).height(3500);
-            });
-
-            // Refresh Skrollr after resizing our sections
-            s.refresh($('.pane'));
-        }
+        }, 100);
     };
 
     app.parallax = {
