81193bb95aed75dac2e19f8fd7be81a601c25d10
Felix Ebert <felix.ebert@if-core.de>
2014-07-17 07:48:17 +0200

    open cartodb modal manually welcome-screen extended clickable area

--- src/js/app.js
+++ src/js/app.js
@@ -23,7 +23,7 @@ var app = {
     app.init = function () {
         if (!Modernizr.touch) {
             $('#slide-0').height($(window).innerHeight());
-            $('.explain-slide').on('click', function () {
+            $('.explain-slide, .intro-box').on('click', function () {
                 $('#slide-0').addClass('off');
             });
             $('html, body, .fullscreen, .fullscreen-content, .pane-scroller, .pane').removeClass('touch').addClass('notouch');
@@ -33,6 +33,18 @@ var app = {
         }
         Object.keys(app.charts).forEach(function (chartId) {
             app.charts[chartId].init();
+        });
+
+        $('#a6-accident-map').modal({
+            show: false
+        });
+        var setupDidRun = false;
+        $('.btn-toggle-accident-map').on('click', function () {
+            if (!setupDidRun) {
+                $('#a6-accident-map .modal-body').html("<iframe class='cartodbmap' src='http://vanessawormer.cartodb.com/viz/045e7644-0ce1-11e4-af53-0e73339ffa50/embed_map?title=true&description=true&search=false&shareable=true&cartodb_logo=true&layer_selector=true&legends=true&scrollwheel=true&fullscreen=true&sublayer_options=1%7C1%7C1%7C1&sql=&sw_lat=49.070717120839156&sw_lon=8.771896362304688&ne_lat=49.34212527586522&ne_lon=9.605484008789062'></iframe>");
+                setupDidRun = true;
+            }
+            $('#a6-accident-map').modal('show');
         });
     };
 })(app, d3);
