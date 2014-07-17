1781be6f27d5c6359b945ad464868948e16c143c
Felix Ebert <felix.ebert@if-core.de>
2014-07-17 02:04:06 +0200

    final desktop version

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,11 +1,32 @@
 (function (skrollr, app) {
     'use strict';
 
+    var skrollrInstance;
+
+    var initScrollObservation = function () {
+        var $body = $('body');
+        var chartList = [];
+        $('.chart').each(function (index, chartElement) {
+            chartList.push($(chartElement));
+        });
+        var treshold = $body.height() - 300;
+        var indexDone = -1;
+        $body.on('scroll', function () {
+            chartList.forEach(function (chart, index) {
+                if (index > indexDone && chart.offset().top < treshold) {
+                    chart.closest('.card').triggerHandler('active');
+                    indexDone++;
+                }
+            });
+        });
+    };
+
     var init = function () {
         setTimeout(function () {
-            var s = skrollr.init({
+            skrollrInstance = skrollr.init({
                 forceHeight: false
             });
+            initScrollObservation();
         }, 100);
     };
 
